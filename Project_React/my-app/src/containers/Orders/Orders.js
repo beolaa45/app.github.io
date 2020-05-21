5eb47d3d5e0eb37e3fbfe196503137c1249101c9
khanh <khanhkango@gmail.com>
2020-05-21 23:30:18 +0700

    app

--- Project_React/my-app/src/containers/Orders/Orders.js
+++ Project_React/my-app/src/containers/Orders/Orders.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
 import axios from '../../axios-oriders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-
+import * as action from '../../Store/actions/index';
+import { connect } from 'react-redux';
+import Spinner from '../../components/UI/Spinner/Spinner'
 class Orders extends Component {
 
-    state = {
-        oriders: [],
-        loading: false
-    }
-
+    // state = {
+    //     oriders: [],
+    //     loading: false
+    // }
+  
     componentDidMount (){
+        this.props.onFetchOrders();
         // this.setState({loading: true})
         // axios.get('/orders.json')
         // .then(res => {
@@ -31,15 +34,33 @@ class Orders extends Component {
         // setTimeout(() => {console.log(this.state)}, 1000)
     }
     render(){
-
-        return (
-            <div>
-                {this.state.oriders.map(order => (
+        console.log(this.props.or)
+        let orders = this.props.loading ?  <Spinner /> : null;
+        if(this.props.or){
+            orders = (
+                <div>
+                {this.props.or.map(order => (
                     <Order  key={order.id} ingredients={order.ingredients} price={order.price}/>
                 ))}
             </div>
-        )
+            )
+        }
+        return orders
+    }
+}
+const mapStateToProps = state => {
+    return {
+       
+        loading: state.order.loading,
+        or: state.order.orders
     }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(action.fetchOrders()),
+        onFetchOrdersStart: () => dispatch(action.fetchOrderStart())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(withErrorHandler(Orders, axios));
--- Project_React/my-app/src/index.js
+++ Project_React/my-app/src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import BurgerBuilderReducer from './Store/reducers/BurgerBuild';
+import burgerBuilderReducer from './Store/reducers/BurgerBuild';
 import thunk from 'redux-thunk';
+import orderReducer from './Store/reducers/order';
+
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+
+})
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(BurgerBuilderReducer,  composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
