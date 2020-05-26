73111f9232913042ff5105f39cea4da46ec32fdc
khanh <khanhkango@gmail.com>
2020-05-26 22:25:04 +0700

    react

--- Project_React/my-app/src/Store/actions/auth.js
+++ Project_React/my-app/src/Store/actions/auth.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as actionTypes from './actionTypes';
-import axios from 'axios'; 
+
 
 export const authStart = () => {
     return {
@@ -24,20 +24,25 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userlId')
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userlId')
     return {
-        type: actionTypes.AUTH_LOGOUT,
+        type: actionTypes.AUTH_INITIATE_LOGOUT,
 
     }
 }
-export const checkAuthTimeout = (expirationTime) => {
-    return dispatch => {
-        setTimeout(() => {
-           dispatch(logout())
-        },expirationTime * 1000)
+
+export const logoutSucceed = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
     }
+}
+export const checkAuthTimeout = (expirationTime) => {
+ return {
+     type: actionTypes.AUTH_CHECK_TIMEOUT,
+     expirationTime: expirationTime
+ }
 }
 
 export const setAuthRedirectPath = (path) => {
@@ -48,51 +53,45 @@ export const setAuthRedirectPath = (path) => {
 }
 
 export const authCheckState = () => {
-    return dispatch => {
-        const token = localStorage.getItem('token');
-        if(!token){
-            dispatch(logout());
-        }else {
-            const expirationDate = new Date(window.localStorage.getItem('expirationDate'));
-            console.log(expirationDate.getDay())
-            if(expirationDate <= new Date()){
-                dispatch(logout())
-            }else {
-                const userlId = localStorage.getItem('userlId')
-                dispatch(authSuccess(token, userlId));
-                // dispatch(checkAuthTimeout(expirationDate.getTime() - new Date().getTime()))
-            }
-        }
-    }
+ return {
+     type: actionTypes.AUTH_CHECK_STATE
+ }
 }
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCI8h5oY2BW7L58ndDRlbNgWysKtaKmKvA";
-        if(!isSignup){
-            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCI8h5oY2BW7L58ndDRlbNgWysKtaKmKvA";
-        }
-        dispatch(authStart())
-        axios.post(url, authData)
-        .then(res => {
-            console.log(res);
-            const expirationDate = new Date( new Date().getTime() + res.data.expiresIn);
-            window.localStorage.setItem('expirationDate', expirationDate);
-            localStorage.setItem('token', res.data.idToken);
-            localStorage.setItem('userlId', res.data.localId)
-            dispatch(authSuccess(res.data.idToken, res.data.localId));
-            dispatch(checkAuthTimeout(res.data.expiresIn))
-        })
-        .catch(error => {
-           
-            dispatch(authFail(error.response.data.error))
-        })
+
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup
     }
+    // return dispatch => {
+    //     const authData = {
+    //         email: email,
+    //         password: password,
+    //         returnSecureToken: true
+    //     }
+    //     let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCI8h5oY2BW7L58ndDRlbNgWysKtaKmKvA";
+    //     if(!isSignup){
+    //         url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCI8h5oY2BW7L58ndDRlbNgWysKtaKmKvA";
+    //     }
+    //     dispatch(authStart())
+    //     axios.post(url, authData)
+    //     .then(res => {
+    //         console.log(res);
+    //         const expirationDate = new Date( new Date().getTime() + res.data.expiresIn);
+    //         window.localStorage.setItem('expirationDate', expirationDate);
+    //         localStorage.setItem('token', res.data.idToken);
+    //         localStorage.setItem('userlId', res.data.localId)
+    //         dispatch(authSuccess(res.data.idToken, res.data.localId));
+    //         dispatch(checkAuthTimeout(res.data.expiresIn))
+    //     })
+    //     .catch(error => {
+           
+    //         dispatch(authFail(error.response.data.error))
+    //     })
+    // }
 }
 
 
--- Project_React/my-app/src/Store/actions/order.js
+++ Project_React/my-app/src/Store/actions/order.js
@@ -30,18 +30,11 @@ import axios from '../../axios-oriders';
      }
  }
  export const purchaseBurgerStart = (orderData, token) => {
-     return dispatch => {
-         dispatch(purchaseBurgerLoading());
-        
-          axios.post('/orders.json?auth=' + token, orderData)
-        .then(res => {
-           dispatch(purchaseBurgerSuccess(res.data.name,orderData))
-        })
-        .catch(error => {
-         dispatch(purchaseBurgerFail(error))
-        });
-   
-     }
+    return {
+        type: actionTypes.PURCHASE_BURGER,
+        orderData: orderData,
+        token: token
+    }
  }
 
 
@@ -67,22 +60,26 @@ import axios from '../../axios-oriders';
  }
 
  export const fetchOrders = (token, userId) => {
-     return dispatch => {
-        dispatch(fetchOrderStart())
-        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo"' + userId + '"';
-        axios.get('/orders.json' + queryParams)
-        .then(res => {
-            const fetcheOrders = [];
-            for(let key in res.data){
-                fetcheOrders.push({
-                    ...res.data[key],
-                    id: key
-                })
-            };
-           dispatch(fetchOrderSuccess(fetcheOrders))
-        })
-        .catch(error => {
-            dispatch(fetchOrderFail(error))
-        })
- }
+     return {
+         type: actionTypes.FETCH_ORDERS_INIT,
+         token: token,
+         userId: userId
+     }
+        // dispatch(fetchOrderStart())
+        // const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo"' + userId + '"';
+        // axios.get('/orders.json' + queryParams)
+        // .then(res => {
+        //     const fetcheOrders = [];
+        //     for(let key in res.data){
+        //         fetcheOrders.push({
+        //             ...res.data[key],
+        //             id: key
+        //         })
+        //     };
+        //    dispatch(fetchOrderSuccess(fetcheOrders))
+        // })
+        // .catch(error => {
+        //     dispatch(fetchOrderFail(error))
+        // })
+ 
 }
