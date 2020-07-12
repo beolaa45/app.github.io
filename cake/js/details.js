791b91802393caa63ce332de5083628b576be704
khanh <khanhkango@gmail.com>
2020-07-12 23:09:17 +0700

    a

--- cake/js/details.js
+++ cake/js/details.js
@@ -1,29 +1,49 @@
-
-$(document).ready(function(){
-    $('.details__carousel-list').slick({
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: false,
-        centerMode: false,
-        cssEase: 'linear',
-        
-       
-    });
-
-    
-    $(".js-details-mini").click(function(){
-        console.log("details")
-
-        if ($(this).hasClass("details-mini--active")) {
-            return console.log("active")
-        }
-
-        $(".details-mini--active").removeClass("details-mini--active");
-        $(this).addClass("details-mini--active")
-
-        let index = $(this).data('inx')
-        console.log(index)
-    })
-
+$(document).ready(function () {
+  $(".details__carousel-list").slick({
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    centerMode: false,
+    cssEase: "linear",
   });
+
+  //// SELECT PHOTO
+
+  $(".js-details-mini").click(function () {
+    console.log("details");
+
+    if ($(this).hasClass("details-mini--active")) {
+      return console.log("active");
+    }
+
+    $(".details-mini--active").removeClass("details-mini--active");
+    $(this).addClass("details-mini--active");
+
+    let index = $(this).attr("data-index");
+
+    let itemContent = $(".details__pho[data-index='" + index + "']");
+    console.log(itemContent);
+    $(".details__photo--active").removeClass("details__photo--active");
+    itemContent.addClass("details__photo--active");
+  });
+
+  /// SELECT NUMBER
+    let count;
+    let numbeBasket = $(".details__number-basket")
+    count = parseInt(numbeBasket.text())
+    console.log(count)
+
+    $(".details__number-plus").click(function() {
+        count++
+        numbeBasket.text(count)
+    })
+    $(".details__number-minus").click(function() {
+        if(count <= 1){
+            return
+        }else{
+            count--;
+            numbeBasket.text(count)
+        }
+    })
+});
