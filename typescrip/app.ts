9840b42c157dd5e3f66b91d2d4ab3da113e2737f
khanh <khanhkango@gmail.com>
2020-08-02 23:21:38 +0700

    app

--- typescrip/app.ts
+++ typescrip/app.ts
@@ -1,4 +1,21 @@
-var user
-    
-user = 1;
-user = "adf"
+class Department {
+    name: String;
+    constructor(n: String){
+        this.name = n
+    }
+
+    static describe(){
+        console.log("Department ");
+    }
+}
+
+class A extends Department {
+    constructor(n: String){
+        super(n)
+
+    }
+}
+
+const obj = new A("khanh")
+console.log(A.describe())
+
